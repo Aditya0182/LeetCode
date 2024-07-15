ae029cceec5369b2ec41eeda900b6ae938e906bb
Adi <41765309+Aditya0182@users.noreply.github.com>
2024-07-16 00:12:16 +0530

    More codes

---
+++ Max Consecutive Ones.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int count= 0 ;
+        int maxcount=0;
+    for(int i = 0;i<nums.length;i++){
+        if(nums[i]==1){
+            count++;
+        }else {
+            maxcount = Math.max(maxcount,count);
+            count = 0;
+        }
+    }
+    return Math.max(maxcount,count);
+    }
+}
