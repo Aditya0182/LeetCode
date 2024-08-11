404eac85e383aeb27459057f8cbe5106f615c4d0
Aditya Chaudhary <aditya.ac2002@gmail.com>
2024-08-11 23:21:25 +0530

    LeetCode

---
+++ Left View of Binary Tree.java
@@ -0,0 +1,23 @@
+class Tree
+{
+    //Function to return list containing elements of left view of binary tree.
+    ArrayList<Integer> leftView(Node root)
+    {
+      // Your code here
+      ArrayList<Integer> res = new ArrayList<>();
+      leftviewHelper(root,0,res);
+      return res;
+      
+      
+    }
+    private void leftviewHelper(Node root,int level, List<Integer> res){
+        if(root ==null){
+            return;
+        }
+        if(res.size()==level){
+            res.add(root.data);
+        }
+        leftviewHelper(root.left,level+1,res);
+         leftviewHelper(root.right,level+1,res);
+    }
+}
